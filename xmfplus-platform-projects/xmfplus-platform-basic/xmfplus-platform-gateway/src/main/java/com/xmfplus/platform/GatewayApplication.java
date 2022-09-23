5ce8f8f44d5e8d58736645ad48042b5b21cfcf33
huanghao <1570356753@qq.com>
2022-09-24 01:22:36 +0800

    project init

---
+++ xmfplus-platform-projects/xmfplus-platform-basic/xmfplus-platform-gateway/src/main/java/com/xmfplus/platform/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.xmfplus.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huanghao
+ * @version 1.0.0
+ * @description
+ * @data 2022/9/24 00:38
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
